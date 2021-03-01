d66553f4f5ee37795d1cdfd1ec2b8f812aa89476
Nitesh <niteshkrsingh51@gmail.com>
2021-03-01 19:53:41 +0530

    Create islistempty.py

---
+++ Lists_programs/islistempty.py
@@ -0,0 +1,13 @@
+#function to check whether a list is empty or not
+def IsListEmpty(num_list):
+    if not num_list:
+        print('The list is empty')
+    else:
+        print('The list is not empty')
+
+   
+num_list = [1]
+#displaying the result
+print(IsListEmpty(num_list))
+    
+      
