1fcf19c6a97ca4c3316321885a66782dc89db268
Nitesh <niteshkrsingh51@gmail.com>
2021-03-04 23:14:27 +0530

    Create password_generators.py

---
+++ misc_programs/password_generators.py
@@ -0,0 +1,25 @@
+#function to generate random passwords
+import random
+def password_generator(user_category):
+    password_data = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+    if user_category == '1':
+        #weak password
+        pass_length = 4
+        new_password = ''.join(random.sample(password_data, pass_length))
+        return new_password
+    elif user_category == '2':
+        #strong password
+        pass_length = 8
+        new_password = ''.join(random.sample(password_data, pass_length))
+        return new_password
+    else:
+        return False
+        
+#displaying the result
+user_category = input('Enter 1 for weak password or 2 for strong password: ')
+result = password_generator(user_category)
+print(result)
+
+
+
+
