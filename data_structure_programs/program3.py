977d2d3b5267d72e5baebd2122c5650c0e99a154
Nitesh <niteshkrsingh51@gmail.com>
2021-03-10 04:47:00 +0530

    Update program3.py

--- data_structure_programs/program3.py
+++ data_structure_programs/program3.py
@@ -2,10 +2,43 @@
 
 def threeEqualChunks(num_list):
 
+    #setting the variables needed
     size = len(num_list)
     chunk_size = size/3
-    
-    start = 0
-    end = chunk_size
+    chunk_size2 = chunk_size + chunk_size
+    chunk_size3 = chunk_size2 + chunk_size
+
+    #initializing the list needed
+    list1 = []
+    list2 = []
+    list3 = []
+
+    #iterating the loop in the list to divide it into 3 chunks
+    for index,items in enumerate(num_list):
+        if index < chunk_size:
+            list1.append(items)
+        elif index < chunk_size2:
+            list2.append(items)
+        elif index <= chunk_size + chunk_size3:
+            list3.append(items)
+
+    #displaying the results        
+    print(f'Original list {num_list}')
+    print(f'Chunk 1 {list1}')
+    list1.reverse()
+    print(f'After reversing it {list1}')
+    print(f'Chunk 2 {list2}')
+    list2.reverse()
+    print(f'After reversing it {list2}')
+    print(f'Chunk 3 {list3}')
+    list3.reverse()
+    print(f'After reversing it {list3}')
+
+#calling the function
+num_list = [11, 45, 8, 23, 14, 12, 78, 45, 89] 
+threeEqualChunks(num_list)
+
+             
+
 
     
