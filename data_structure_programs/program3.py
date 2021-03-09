388fa24cf5254dc43835394bd3246c786c4f3201
Nitesh <niteshkrsingh51@gmail.com>
2021-03-10 01:56:27 +0530

    Create program3.py

---
+++ data_structure_programs/program3.py
@@ -0,0 +1,11 @@
+#Given a list slice it into 3 equal chunks and reverse each chunk
+
+def threeEqualChunks(num_list):
+
+    size = len(num_list)
+    chunk_size = size/3
+    
+    start = 0
+    end = chunk_size
+
+    
